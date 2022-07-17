c01bd18cca2e9060ac38661f496349df18717768
Umbreon <umbreon@disroot.org>
2022-07-18 00:25:32 +0100

    Added way to quit

--- scripts/play_music_mobile.sh
+++ scripts/play_music_mobile.sh
@@ -4,5 +4,6 @@ termux-notification \
     --media-next $'echo \'{"command": ["playlist-next"]}\' | socat - ~/mpvsocket' \
     --media-pause $'echo \'{"command": ["cycle", "pause"]}\' | socat - ~/mpvsocket' \
     --media-play $'echo \'{"command": ["cycle", "pause"]}\' | socat - ~/mpvsocket' \
-    --media-previous $'echo \'{"command": ["playlist-prev"]}\' | socat - ~/mpvsocket'
+    --media-previous $'echo \'{"command": ["playlist-prev"]}\' | socat - ~/mpvsocket' \
+    --on-delete $'echo \'{"command": ["quit"]}\' | socat - ~/mpvsocket'
 jq -r '"ytdl://\(.[0].videos[].videoId)"' ~/.dotfiles/freetube/playlists.db  | mpv --playlist=- --vid=no --shuffle --input-ipc-server=~/mpvsocket
