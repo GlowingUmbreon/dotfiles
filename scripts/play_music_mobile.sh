c64c562fe6f65de446419e4d5e91cae22e3b9280
Umbreon <umbreon@disroot.org>
2022-07-18 00:12:05 +0100

    .

--- scripts/play_music_mobile.sh
+++ scripts/play_music_mobile.sh
@@ -1,3 +1,2 @@
-jq -r '"ytdl://\(.[0].videos[].videoId)"' ~/.dotfiles/freetube/playlists.db  | mpv --playlist=- --vid=no --shuffle --input-ipc-server=~/mpvsocket
-
 termux-notification --type 'media' --alert-once --media-next $'echo \'{"command": ["playlist-next"]}\' > ~/mpvsocket' --media-pause $'echo \'{"command": ["cycle", "pause"]}\' > ~/mpvsocket' --media-play $'echo \'{"command": ["cycle", "pause"]}\' > ~/mpvsocket' --media-previous $'echo \'{"command": ["playlist-prev"]}\' > ~/mpvsocket'
+jq -r '"ytdl://\(.[0].videos[].videoId)"' ~/.dotfiles/freetube/playlists.db  | mpv --playlist=- --vid=no --shuffle --input-ipc-server=~/mpvsocket
