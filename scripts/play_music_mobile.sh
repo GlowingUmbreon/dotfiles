b393065ea4596ddc453a439dd6018fcf9ff65dc2
Umbreon <umbreon@disroot.org>
2022-07-18 00:20:27 +0100

    made a mistake

--- scripts/play_music_mobile.sh
+++ scripts/play_music_mobile.sh
@@ -1,2 +1,8 @@
-termux-notification --type 'media' --alert-once --media-next $'echo \'{"command": ["playlist-next"]}\' > ~/mpvsocket' --media-pause $'echo \'{"command": ["cycle", "pause"]}\' > ~/mpvsocket' --media-play $'echo \'{"command": ["cycle", "pause"]}\' > ~/mpvsocket' --media-previous $'echo \'{"command": ["playlist-prev"]}\' > ~/mpvsocket'
+termux-notification \
+    --type 'media' \
+    --alert-once \
+    --media-next $'echo \'{"command": ["playlist-next"]}\' | socat - ~/mpvsocket' \
+    --media-pause $'echo \'{"command": ["cycle", "pause"]}\' | socat - ~/mpvsocket' \
+    --media-play $'echo \'{"command": ["cycle", "pause"]}\' | socat - ~/mpvsocket' \
+    --media-previous $'echo \'{"command": ["playlist-prev"]}\' | socat - ~/mpvsocket'
 jq -r '"ytdl://\(.[0].videos[].videoId)"' ~/.dotfiles/freetube/playlists.db  | mpv --playlist=- --vid=no --shuffle --input-ipc-server=~/mpvsocket
